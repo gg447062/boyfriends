7203a80a3e8ce38dfc9fcc229f07225321cadb13
Gabriel Gutierrez <gg447062@gmail.com>
2022-01-22 15:13:12 -0600

    adds connect wallet function

--- app/Components/Meat.js
+++ app/Components/Meat.js
@@ -1,23 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
 
 const Meat = () => {
+  const [disabled, setDisabled] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [mintingMessage, setMintingMessage] = useState('');
+  const { ethereum } = window;
+
+  const getAccount = async () => {
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    setAccount(accounts[0]);
+  };
+
+  const connect = async () => {
+    if (ethereum) {
+      try {
+        setDisabled(true);
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        getAccount();
+      } catch (error) {
+        alert(error.message);
+        setDisabled(false);
+      }
+    } else {
+      alert('Please install metamask!');
+    }
+  };
+
+  const mint = () => {
+    setMintingMessage('minting...');
+    setTimeout(() => {
+      setMintingMessage('success!');
+    }, 2000);
+  };
+
+  useEffect(async () => {
+    if (ethereum.selectedAddress) {
+      setDisabled(true);
+      getAccount();
+    }
+  }, []);
+
   return (
     <div className="meat">
       <div className="icon-container">
         <img className="icons" src="assets/images/mouse.jpg"></img>
-        <div>connect wallet</div>
+        <button onClick={connect} disabled={disabled}>
+          {account ? 'connected' : 'connect wallet'}
+        </button>
       </div>
       <div className="icon-container">
         <img className="icons" src="assets/images/head.jpg"></img>
-        <div>mint.eth</div>
+        <button onClick={mint}>mint.eth</button>
       </div>
       <div className="icon-container">
         <img className="icons" src="assets/images/face.jpg"></img>
-        <div>darbytrash.link</div>
+        <button>darbytrash.link</button>
       </div>
       <div className="icon-container">
         <img className="computer" src="assets/images/computer.png"></img>
-        <div id="minting">minting...</div>
+        <div id="minting">{mintingMessage}</div>
       </div>
       <div className="sidebar">
         <div className="square up"></div>
--- app/Components/Minter.js
+++ app/Components/Minter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Meat from './Meat';
 import Info from './Info';
 import Footer from './Footer';
